88a5067d56866be482bf056c451d8ed23e443be7
unknown <mazl001@foxmail.com>
2021-07-25 16:34:39 +0800

    初始化

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,8 @@
+<?php
+namespace app\Http\Controllers;
+
+class IndexController {
+    public function index() {
+        return 'hello world';
+    }
+}
---
+++ config/provider.php
@@ -0,0 +1,17 @@
+<?php
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | 框架自动加载的服务提供者类
+    |--------------------------------------------------------------------------
+    */
+    Onion\Providers\EventServiceProvider::class,
+    Onion\Providers\CookieServiceProvider::class,
+	Onion\Providers\DatabaseServiceProvider::class,
+	Onion\Providers\EncryptionServiceProvider::class,
+	Onion\Providers\RedisServiceProvider::class,
+	Onion\Providers\RouteServiceProvider::class,
+	Onion\Providers\SessionServiceProvider::class,
+	Onion\Providers\ViewServiceProvider::class,
+];
